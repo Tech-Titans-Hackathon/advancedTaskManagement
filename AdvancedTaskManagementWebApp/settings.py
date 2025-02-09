930d8cfacb42db6d080302166a14d0aa4316e545
imshn <shaan@vyomr.in>
2025-02-09 23:25:33 +0530

    Setup done

--- AdvancedTaskManagementWebApp/settings.py
+++ AdvancedTaskManagementWebApp/settings.py
@@ -136,12 +136,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  # For development
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Where collectstatic will store files in production
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
